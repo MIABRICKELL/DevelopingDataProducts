cc286dc535a4bdbb48519adc2a9df51014fb0825
eruda <erik.ruda@hotmail.com>
2015-02-22 17:18:07 -0500

    Initial Project Submission
    Initial Project Submission

---
+++ server.R
@@ -0,0 +1,67 @@
+library(shiny)
+
+#calculate the number of new cases
+newcases <- function(start, end) {start - end}
+
+#calculate the Person-Time at risk estimate
+risktime <- function(begrisk, endrisk, timeunits)
+{
+        (begrisk+endrisk/2) * timeunits
+}
+
+#function to convert to format a decimal to a percentage
+percent <- function(x, digits = 2, format = "f", ...) {
+        paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+shinyServer(
+        function(input, output) {
+                #return the name of the disease
+                getdisease <-reactive(
+                        {if(input$disease=='1') "malaria"
+                         else if (input$disease=='2') "flu"
+                         else if (input$disease=='3') "ebola"
+                         else if (input$disease=='4') "measles"})
+                
+                #retun the name of the time type with correct format
+                gettype <-reactive(
+                        {if(input$timetype=='1') "day"
+                         else if (input$timetype=='2') "month"
+                         else if (input$timetype=='3') "year"})
+                         
+                
+                gettypef <-reactive(
+                        {if(input$timetype=='1' && input$timeunits==1) "day"
+                         else if(input$timetype=='1' && input$timeunits>1) "days"
+                         else if (input$timetype=='2' && input$timeunits==1) "month"
+                         else if (input$timetype=='2' && input$timeunits>1) "months"
+                         else if (input$timetype=='3' && input$timeunits==1) "year"
+                         else if (input$timetype=='3' && input$timeunits>1) "years"})
+                
+                #retun # of time units if greater than zeo
+                gettimeunits <-reactive(
+                        {if(input$timeunits>0) input$timeunits else ""})
+                
+                #reactive function to calculate the new cases
+                ncases <- reactive({newcases(input$BegAtRisk, input$EndAtRisk)})
+                
+                #reactive function to calculate the person-time at risk
+                rtime <- reactive({risktime(input$BegAtRisk, input$EndAtRisk, input$timeunits)})
+                
+                output$newcases <- renderPrint({
+                        if(input$submitButton == 0) ""
+                           else ncases()})
+                output$timeatrisk <- renderPrint({
+                        if(input$submitButton == 0) ""
+                           else rtime()})
+                output$timeinterval <-renderPrint({
+                        if(input$submitButton == 0) ""
+                        else paste0(gettimeunits(), " ", gettypef())})
+                output$rate <- renderPrint({
+                        if(input$submitButton == 0) "Need to hit submit button."
+                        else 
+                        if(rtime()>0) paste0(percent(ncases()/rtime()), " of the people are getting ", getdisease(), " each ", gettype(), ".")
+                        else paste0("None of the people are getting ", getdisease(), " or time needs to be more than 0.")})
+                
+        }
+)
---
+++ ui.R
@@ -0,0 +1,29 @@
+shinyUI(
+        pageWithSidebar(
+                # Application title
+                headerPanel("Incidence Rate Calculator"),
+                sidebarPanel(
+                        h4('To determine the estimated incidence rate of a disease for a specific population. Please select a disease from the list and enter 
+                            the number of people at risk at the beginning of the time interval along the number of people at risk at the end of the time interval,
+                            and the measure of time.'),
+                        selectInput("disease", "Choose disease:", choices=c('malaria'=1, 'flu'=2, 'ebola'=3, 'measles'=4)),
+                        numericInput('BegAtRisk', 'Number of people at risk at beginning of time period:', 100, min = 10),
+                        numericInput('EndAtRisk', 'Number of people at risk at end of time period \n
+                                     (# above - # developed disease):', 0, min = 0),
+                        numericInput('timeunits', 'Number of time periods', 0, min = 50, max = 200, step = 5),
+                        selectInput("timetype", "Choose time period:", choices=c('days'=1, 'months'=2, 'years'=3)),
+                        actionButton("submitButton", "Submit")
+                ),
+                mainPanel(
+                        h4('New Cases'),
+                        verbatimTextOutput("newcases"),
+                        h4('Person Time at Risk '),
+                        verbatimTextOutput("timeatrisk"),
+                        h4('Time Interval '),
+                        verbatimTextOutput("timeinterval"),
+                        h4('Incidence Rate '),
+                        verbatimTextOutput("rate")
+                        
+                )
+        )
+)
